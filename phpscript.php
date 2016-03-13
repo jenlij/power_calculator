254a488c0fc5c344fd37bc7c2daac2f7ac7e5c0a
jenlij <jljohns1216@gmail.com>
2016-03-13 17:46:02 -0400

    updated format

--- phpscript.php
+++ phpscript.php
@@ -13,58 +13,81 @@
 	//command line input
         $cmdstr = '/usr/cluster/bin/CaTS';
         $cmdstr .= ' --cases '.$ncases.' --controls '. $ncontrols.' --pisamples '. $pisamples.' --pimarkers '.$pimarkers.' --freq '. $freq.' --risk '.$risk.' --prevalence '.$prevalence.' --alpha '.$alpha;
-//      $output = shell_exec($cmdstr);
-//      print $output;
         $outVal = exec($cmdstr);
-        //$filename = '/net/wonderland/home/jenlij/CatsOutData.txt';
-        //$data = file($filename);
+      
 	$error = "I don't like the genetic model you requested!";
-        if($outVal == $error) {
+      if($outVal == $error) {
                 echo"<font color='#FF0000'>I don't like the genetic model you requested!</font>";
                 echo nl2br("\n");
                 print "<font color='#FF0000'>Please try setting new parameters.</font>";
-        }
-        else {
-	//http://papermashup.com/using-php-and-css-to-make-a-simple-graph/	
-	$data = explode(" ", $outVal);
-	$percent1 = $data[1] * 100;
-	$percent2 = $data[3] * 100;
-	$percent3 = $data[5] * 100;
-
-	echo "<strong>Probability of disease:</strong>";
-        echo nl2br("\n");
+      }
+      //http://papermashup.com/using-php-and-css-to-make-a-simple-graph/  
+      else {   
+	         $data = explode(" ", $outVal);
+	         $percent1 = $data[1] * 100;
+	         $percent2 = $data[3] * 100;
+	         $percent3 = $data[5] * 100;
+           $powerpercent = $data[8] * 100;
+           $casespercent = $data[6] * 100;
+           $controlspercent = $data[7] *100;
+        
         echo '
         <table cellspacing="5">
-	<tr>
+        <tr>
+        <td>
+            <strong>Expected power for a one stage study:</strong> 
+        </td>
+        <td>
+            <div class="rating graphcont graph1"><strong class="bar" style="width:',$powerpercent,'%;">',$data[8],'</strong></div><div class="clear"></div> 
+        </td>
+        </tr>
+        <tr><td>&nbsp</td></tr>
+
+        <tr>
+        <td><strong>Expected disease allele frequency:</strong></td>
+        </tr>
+        <tr>
+        <td>Cases:</td>
+        <td>
+          <div class="rating graphcont graph2"><strong class="bar" style="width:',$casespercent,'%;">',$data[6],'</strong></div><div class="clear"></div>
+        </td>
+        </tr>
+        <tr>
+        <td>Controls:</td>
+        <td>
+          <div class="rating graphcont graph2"><strong class="bar" style="width:',$controlspercent,'%;">',$data[7],'</strong></div><div class="clear"></div>
+        </td>
+        </tr>
+        <tr><td>&nbsp</td></tr>
+
+        <tr>
+        <td><strong>Probability of disease:</strong></td>
+        </tr>
+        <tr>
         <td>Genotype A/A [with frequency ',$data[0],']:</td>
-        <td><div class="rating"><div class="graphcont"><div class="graph"><strong class="bar" style="width:',$percent1,'%;">',$percent1,'%</strong></div></div></div><div class="clear"></div></td>
+        <td><div class="rating graphcont graph3"><strong class="bar" style="width:',$percent1,'%;">',$percent1,'%</strong></div><div class="clear"></div></td>
         </tr>
         <tr>
         <td>Genotype A/B [with frequency ',$data[2],']:</td>
-        <td><div class="rating"><div class="graphcont"><div class="graph"><strong class="bar" style="width:',$percent2,'%;">',$percent2,'%</strong></div></div></div><div class="clear"></div></td>
+        <td><div class="rating graphcont graph3"><strong class="bar" style="width:',$percent2,'%;">',$percent2,'%</strong></div><div class="clear"></div></td>
         </tr>
         <tr>
         <td>Genotype B/B [with frequency ',$data[4],']:</td>
-        <td><div class="rating"><div class="graphcont"><div class="graph"><strong class="bar" style="width:',$percent3,'%;">',$percent3,'%</strong></div></div></div><div class="clear"></div></td>
+        <td><div class="rating graphcont graph3"><strong class="bar" style="width:',$percent3,'%;">',$percent3,'%</strong></div><div class="clear"></div></td>
         </tr>
         </table>';
+  
+  }
 
-	echo nl2br("\n");
-	echo "<strong>Expected disease allele frequency:</strong>";
-	echo nl2br("\n");
-	print "Cases: <strong> ".$data[6];
-	echo nl2br("\n");
-	print "</strong>Controls: <strong>".$data[7];
-	echo nl2br("\n");
-	echo nl2br("\n");
-	echo "Expected power for a one stage study: ".$data[8];
-	}
-	echo nl2br("\n");	
-	echo nl2br("\n");
-	print "--------------------------------------------------";
-	echo nl2br("\n");	
-	echo nl2br("\n");
-	print "Parameters in effect:";
-        echo nl2br("\n"); 
-        print "cases: ".$ncases.", controls: ".$ncontrols.", significance level: ".$alpha.", prevalence: ".$prevalence.", disease allele frequency: ".$freq.", genotype relative risk: ".$risk;
+  echo nl2br("\n"); 
+  echo nl2br("\n");
+  echo '<table cellspacing="5">
+  <tr>
+  <td><strong>Parameters in effect:</strong></td>
+  </tr> 
+  <tr>
+    <td>cases: ',$ncases,', controls: ',$ncontrols,', significance level: ',$alpha,', prevalence: ',$prevalence,', disease allele frequency: ',$freq,', genotype relative risk: ',$risk,'
+    </td>
+  </tr>
+  </table>';
 ?>
